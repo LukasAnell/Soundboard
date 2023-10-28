d4e4cd054f023e94b1b2545d2eb290aef0d2ccf5
Lukas Anell <lukasmanell@gmail.com>
2023-10-27 22:14:04 -0700

    Added new files that cover almost the full scale, removed old ones. Rewrote initializeSoundPool() to include the new notes.

--- app/src/main/java/com/mistershorr/soundboard/MainActivity.kt
+++ app/src/main/java/com/mistershorr/soundboard/MainActivity.kt
@@ -31,31 +31,75 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonPlaySong: Button
     lateinit var groupMainNoteButtons: Group
     lateinit var soundPool: SoundPool
-    var aNote = 0
-    var bbNote = 0
-    var bNote = 0
-    var cNote = 0
-    var cSharpNote = 0
-    var dNote = 0
-    var dSharpNote = 0
-    var eNote = 0
-    var fNote = 0
-    var fSharpNote = 0
-    var gNote = 0
-    var gSharpNote = 0
-    var lowGNote = 0
-    var highANote = 0
-    var highBNote = 0
-    var highBbNote = 0
-    var highCNote = 0
-    var highCSharpNote = 0
-    var highDNote = 0
-    var highDSharpNote = 0
-    var highENote = 0
-    var highFNote = 0
-    var highFSharpNote = 0
-    var highGNote = 0
-    var highGSharpNote = 0
+
+//    var A0 = 0
+//    var A1 = 0
+//    var A2 = 0
+    var A3 = 0
+//    var A4 = 0
+//    var A5 = 0
+//    var A7 = 0
+//    var B1 = 0
+//    var B2 = 0
+    var B3 = 0
+//    var B4 = 0
+//    var B5 = 0
+//    var B7 = 0
+//    var Bb1 = 0
+//    var Bb2 = 0
+    var Bb3 = 0
+//    var Bb4 = 0
+//    var Bb5 = 0
+//    var Bb7 = 0
+//    var C1 = 0
+//    var C2 = 0
+//    var C3 = 0
+    var C4 = 0
+//    var C5 = 0
+//    var C6 = 0
+//    var C8 = 0
+//    var CSharp2 = 0
+//    var CSharp3 = 0
+    var CSharp4 = 0
+//    var CSharp5 = 0
+//    var D1 = 0
+//    var D2 = 0
+//    var D3 = 0
+    var D4 = 0
+//    var D5 = 0
+//    var DSharp1 = 0
+//    var DSharp2 = 0
+//    var DSharp3 = 0
+    var DSharp4 = 0
+//    var DSharp5 = 0
+//    var E1 = 0
+//    var E2 = 0
+//    var E3 = 0
+    var E4 = 0
+//    var E5 = 0
+//    var F1 = 0
+//    var F2 = 0
+//    var F3 = 0
+    var F4 = 0
+//    var F5 = 0
+//    var FSharp0 = 0
+//    var FSharp1 = 0
+//    var FSharp2 = 0
+//    var FSharp3 = 0
+    var FSharp4 = 0
+//    var FSharp5 = 0
+//    var G0 = 0
+//    var G1 = 0
+//    var G2 = 0
+//    var G3 = 0
+    var G4 = 0
+//    var G5 = 0
+//    var GSharp0 = 0
+//    var GSharp1 = 0
+//    var GSharp2 = 0
+//    var GSharp3 = 0
+    var GSharp4 = 0
+//    var GSharp5 = 0
 
     lateinit var binding: ActivityMainBinding
 
@@ -177,57 +221,74 @@ class MainActivity : AppCompatActivity() {
 //           // isSoundPoolLoaded = true
 //        })
 
-        aNote = soundPool.load(this, R.raw.scalea, 1)
-        bbNote = soundPool.load(this, R.raw.scalebb, 1)
-        bNote = soundPool.load(this, R.raw.scaleb, 1)
-        cNote =  soundPool.load(this, R.raw.scalec, 1)
-        cSharpNote = soundPool.load(this, R.raw.scalecs, 1)
-        dNote = soundPool.load(this, R.raw.scaled, 1)
-        dSharpNote = soundPool.load(this, R.raw.scaleds, 1)
-        eNote = soundPool.load(this, R.raw.scalee, 1)
-        fNote = soundPool.load(this, R.raw.scalef, 1)
-        fSharpNote = soundPool.load(this, R.raw.scalefs, 1)
-        gNote = soundPool.load(this, R.raw.scaleg, 1)
-        gSharpNote = soundPool.load(this, R.raw.scalegs, 1)
-        lowGNote = soundPool.load(this, R.raw.scalelowg, 1)
-        highANote = soundPool.load(this, R.raw.scalehigha, 1)
-        highBNote = soundPool.load(this, R.raw.scalehighb, 1)
-        highBbNote = soundPool.load(this, R.raw.scalehighbb, 1)
-        highCNote = soundPool.load(this, R.raw.scalehighc, 1)
-        highCSharpNote = soundPool.load(this, R.raw.scalehighcs, 1)
-        highDNote = soundPool.load(this, R.raw.scalehighd, 1)
-        highDSharpNote = soundPool.load(this, R.raw.scalehighds, 1)
-        highENote = soundPool.load(this, R.raw.scalehighe, 1)
-        highFNote = soundPool.load(this, R.raw.scalehighf, 1)
-        highFSharpNote = soundPool.load(this, R.raw.scalehighfs, 1)
-        highGNote = soundPool.load(this, R.raw.scalehighg, 1)
-        highGSharpNote = soundPool.load(this, R.raw.scalehighgs, 1)
-
-        noteMap["A"] = aNote
-        noteMap["Bb"] = bbNote
-        noteMap["B"] = bNote
-        noteMap["C"] = cNote
-        noteMap["C#"] = cSharpNote
-        noteMap["D"] = dNote
-        noteMap["D#"] = dSharpNote
-        noteMap["E"] = eNote
-        noteMap["F"] = fNote
-        noteMap["F#"] = fSharpNote
-        noteMap["G"] = gNote
-        noteMap["G#"] = gSharpNote
-        noteMap["LG"] = lowGNote
-        noteMap["HA"] = highANote
-        noteMap["HB"] = highBNote
-        noteMap["HBb"] = highBbNote
-        noteMap["HC"] = highCNote
-        noteMap["HC#"] = highCSharpNote
-        noteMap["HD"] = highDNote
-        noteMap["HD#"] = highDSharpNote
-        noteMap["HE"] = highENote
-        noteMap["HF"] = highFNote
-        noteMap["HF#"] = highFSharpNote
-        noteMap["HG"] = highGNote
-        noteMap["HG#"] = highGSharpNote
+        noteMap["A0"] = soundPool.load(this, R.raw.A0, 1)
+        noteMap["A1"] = soundPool.load(this, R.raw.A1, 1)
+        noteMap["A2"] = soundPool.load(this, R.raw.A2, 1)
+        noteMap["A3"] = soundPool.load(this, R.raw.A3, 1)
+        noteMap["A4"] = soundPool.load(this, R.raw.A4, 1)
+        noteMap["A5"] = soundPool.load(this, R.raw.A5, 1)
+        noteMap["A7"] = soundPool.load(this, R.raw.A7, 1)
+        noteMap["B1"] = soundPool.load(this, R.raw.B1, 1)
+        noteMap["B2"] = soundPool.load(this, R.raw.B2, 1)
+        noteMap["B3"] = soundPool.load(this, R.raw.B3, 1)
+        noteMap["B4"] = soundPool.load(this, R.raw.B4, 1)
+        noteMap["B5"] = soundPool.load(this, R.raw.B5, 1)
+        noteMap["B7"] = soundPool.load(this, R.raw.B7, 1)
+        noteMap["Bb1"] = soundPool.load(this, R.raw.Bb1, 1)
+        noteMap["Bb2"] = soundPool.load(this, R.raw.Bb2, 1)
+        noteMap["Bb3"] = soundPool.load(this, R.raw.Bb3, 1)
+        noteMap["Bb4"] = soundPool.load(this, R.raw.Bb4, 1)
+        noteMap["Bb5"] = soundPool.load(this, R.raw.Bb5, 1)
+        noteMap["Bb7"] = soundPool.load(this, R.raw.Bb7, 1)
+        noteMap["C1"] = soundPool.load(this, R.raw.C1, 1)
+        noteMap["C2"] = soundPool.load(this, R.raw.C2, 1)
+        noteMap["C3"] = soundPool.load(this, R.raw.C3, 1)
+        noteMap["C4"] = soundPool.load(this, R.raw.C4, 1)
+        noteMap["C5"] = soundPool.load(this, R.raw.C5, 1)
+        noteMap["C6"] = soundPool.load(this, R.raw.C6, 1)
+        noteMap["C8"] = soundPool.load(this, R.raw.C8, 1)
+        noteMap["CSharp2"] = soundPool.load(this, R.raw.CSharp2, 1)
+        noteMap["CSharp3"] = soundPool.load(this, R.raw.CSharp3, 1)
+        noteMap["CSharp4"] = soundPool.load(this, R.raw.CSharp4, 1)
+        noteMap["CSharp5"] = soundPool.load(this, R.raw.CSharp5, 1)
+        noteMap["D1"] = soundPool.load(this, R.raw.D1, 1)
+        noteMap["D2"] = soundPool.load(this, R.raw.D2, 1)
+        noteMap["D3"] = soundPool.load(this, R.raw.D3, 1)
+        noteMap["D4"] = soundPool.load(this, R.raw.D4, 1)
+        noteMap["D5"] = soundPool.load(this, R.raw.D5, 1)
+        noteMap["DSharp1"] = soundPool.load(this, R.raw.DSharp1, 1)
+        noteMap["DSharp2"] = soundPool.load(this, R.raw.DSharp2, 1)
+        noteMap["DSharp3"] = soundPool.load(this, R.raw.DSharp3, 1)
+        noteMap["DSharp4"] = soundPool.load(this, R.raw.DSharp4, 1)
+        noteMap["DSharp5"] = soundPool.load(this, R.raw.DSharp5, 1)
+        noteMap["E1"] = soundPool.load(this, R.raw.E1, 1)
+        noteMap["E2"] = soundPool.load(this, R.raw.E2, 1)
+        noteMap["E3"] = soundPool.load(this, R.raw.E3, 1)
+        noteMap["E4"] = soundPool.load(this, R.raw.E4, 1)
+        noteMap["E5"] = soundPool.load(this, R.raw.E5, 1)
+        noteMap["F1"] = soundPool.load(this, R.raw.F1, 1)
+        noteMap["F2"] = soundPool.load(this, R.raw.F2, 1)
+        noteMap["F3"] = soundPool.load(this, R.raw.F3, 1)
+        noteMap["F4"] = soundPool.load(this, R.raw.F4, 1)
+        noteMap["F5"] = soundPool.load(this, R.raw.F5, 1)
+        noteMap["FSharp0"] = soundPool.load(this, R.raw.FSharp0, 1)
+        noteMap["FSharp1"] = soundPool.load(this, R.raw.FSharp1, 1)
+        noteMap["FSharp2"] = soundPool.load(this, R.raw.FSharp2, 1)
+        noteMap["FSharp3"] = soundPool.load(this, R.raw.FSharp3, 1)
+        noteMap["FSharp4"] = soundPool.load(this, R.raw.FSharp4, 1)
+        noteMap["FSharp5"] = soundPool.load(this, R.raw.FSharp5, 1)
+        noteMap["G0"] = soundPool.load(this, R.raw.G0, 1)
+        noteMap["G1"] = soundPool.load(this, R.raw.G1, 1)
+        noteMap["G2"] = soundPool.load(this, R.raw.G2, 1)
+        noteMap["G3"] = soundPool.load(this, R.raw.G3, 1)
+        noteMap["G4"] = soundPool.load(this, R.raw.G4, 1)
+        noteMap["G5"] = soundPool.load(this, R.raw.G5, 1)
+        noteMap["GSharp0"] = soundPool.load(this, R.raw.GSharp0, 1)
+        noteMap["GSharp1"] = soundPool.load(this, R.raw.GSharp1, 1)
+        noteMap["GSharp2"] = soundPool.load(this, R.raw.GSharp2, 1)
+        noteMap["GSharp3"] = soundPool.load(this, R.raw.GSharp3, 1)
+        noteMap["GSharp4"] = soundPool.load(this, R.raw.GSharp4, 1)
+        noteMap["GSharp5"] = soundPool.load(this, R.raw.GSharp5, 1)
     }
 
     private fun playNote(noteId: Int) {
@@ -237,18 +298,18 @@ class MainActivity : AppCompatActivity() {
     private inner class SoundBoardListener : View.OnClickListener {
         override fun onClick(v: View?) {
             when(v?.id) {
-                R.id.button_main_a -> playNote(aNote)
-                R.id.button_main_bb -> playNote(bbNote)
-                R.id.button_main_b -> playNote(bNote)
-                R.id.button_main_c -> playNote(cNote)
-                R.id.button_main_cSharp -> playNote(cSharpNote)
-                R.id.button_main_d -> playNote(dNote)
-                R.id.button_main_dSharp -> playNote(dSharpNote)
-                R.id.button_main_e -> playNote(eNote)
-                R.id.button_main_f -> playNote(fNote)
-                R.id.button_main_fSharp -> playNote(fSharpNote)
-                R.id.button_main_g -> playNote(gNote)
-                R.id.button_main_gSharp -> playNote(gSharpNote)
+                R.id.button_main_a -> playNote(A3)
+                R.id.button_main_bb -> playNote(Bb3)
+                R.id.button_main_b -> playNote(B3)
+                R.id.button_main_c -> playNote(C4)
+                R.id.button_main_cSharp -> playNote(CSharp4)
+                R.id.button_main_d -> playNote(D4)
+                R.id.button_main_dSharp -> playNote(DSharp4)
+                R.id.button_main_e -> playNote(E4)
+                R.id.button_main_f -> playNote(F4)
+                R.id.button_main_fSharp -> playNote(FSharp4)
+                R.id.button_main_g -> playNote(G4)
+                R.id.button_main_gSharp -> playNote(GSharp4)
             }
         }
     }
